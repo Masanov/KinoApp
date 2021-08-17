e94012f5a4d29f178e7c1a7ba5dcc0261fb23999
Masanov <masanav.ag@gmail.com>
2021-08-17 22:36:17 +0300

    Выполнение итогового задания модуля 21.

--- app/src/main/java/com/devmasanov/kinoapp/MainActivity.kt
+++ app/src/main/java/com/devmasanov/kinoapp/MainActivity.kt
@@ -2,10 +2,22 @@ package com.devmasanov.kinoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
+//    private lateinit var binding: TopAppBarBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        binding = TopAppBarBinding.inflate(layoutInflater)
+//        val view = binding.root
     }
+
+    fun onClickToast(view: View) {
+        Toast.makeText(this, "Hi Great! Somebody told me you have a boyfriend",Toast.LENGTH_SHORT).show()
+    }
+
 }
